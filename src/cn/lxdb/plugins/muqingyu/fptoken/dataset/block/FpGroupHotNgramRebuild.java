55912a09b4505938f256efb185609abdc020b706
muqingyu2015 <13998868475@163.com>
2026-05-22 18:38:19 +0800

    重构代码

--- src/cn/lxdb/plugins/muqingyu/fptoken/dataset/block/FpGroupHotNgramRebuild.java
+++ src/cn/lxdb/plugins/muqingyu/fptoken/dataset/block/FpGroupHotNgramRebuild.java
@@ -1,7 +1,6 @@
 package cn.lxdb.plugins.muqingyu.fptoken.dataset.block;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -49,20 +48,7 @@ import cn.lxdb.plugins.muqingyu.fptoken.dataset.common.FpTermKey;
  */
 public final class FpGroupHotNgramRebuild {
 
-	/** 锚点分档索引：{@code termsByByteLength.get(byteLen)} 为该字节长度档内的去重热词/子串（有上限）。 */
-	private static final class AnchorTierIndex extends ArrayList<TreeSet<FpTermKey>> {
-		private static final long serialVersionUID = 1L;
-
-		AnchorTierIndex() {
-			super(Lucene80FPSearchConfig.NGRAM_MAX + 1);
-			for (int byteLen = 0; byteLen <= Lucene80FPSearchConfig.NGRAM_MAX; byteLen++) {
-				add(new TreeSet<>());
-			}
-		}
-	}
-
-	private FpGroupHotNgramRebuild() {
-	}
+	
 
 	/**
 	 * 热词重建主流程，结果写入 {@code group} 的热词表与 {@code hotTermToLevel}。
